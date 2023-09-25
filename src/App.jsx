9170b8a5795bae44900b51a8af0f648703542edb
Zan-14 <fauzanjumdinharm@gmail.com>
2023-09-25 22:41:04 +0700

    Header-Footer-Kandidat

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/home-page";
-import LoginPage from "./pages/Login-page";
-import RegisterPage from "./pages/register-page";
-import NotFound from "./pages/404";
-import ProfilePage from "./pages/profile-page";
-import HomePageKandidat from "./pages/home-page-kandidat";
+import { BrowserRouter } from "react-router-dom";
+
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/Register" element={<RegisterPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="*" element={<NotFound />} />
-        <Route path="/kandidat" element={<HomePageKandidat />} />
-      </Routes>
-    </Router>
-  );
+  return <BrowserRouter></BrowserRouter>;
 };
 
 export default App;
