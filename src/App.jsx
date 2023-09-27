bea81210f48da2258a67efa3e83e7327f473968f
AnnangBudiS <GGwarriorNet@gmail.com>
2023-09-27 20:13:57 +0700

    fix app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { NavbarKandidat } from "./components/Navbar/Navbar";
-import { FooterKandidat } from "./components/Footer/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Footer } from "./components/Footer/Footer";
 
 const App = () => {
   return (
@@ -9,7 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/Register" element={<RegisterPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/profile" element={<ProfilePage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
